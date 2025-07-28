971d45483dc1f0d801dbe0bad346a609f41f5e28
IgorVeto4kin <gregnesterovitch@gmail.com>
2025-07-28 13:04:26 +0300

    install dependencies correct now

--- lazy_setup.sh
+++ lazy_setup.sh
@@ -3,6 +3,7 @@ sudo apt update -y
 sudo apt install -y qt6-base-dev 
 sudo apt install -y qt6-tools-dev-tools
 sudo apt install -y qt6-tools-dev
+sudo apt install -y libqt4-dev
 #sudo apt install -y qt6-network-dev # unable to locate this package, idk why
 #sudo apt install -y
 #sudo apt install -y
